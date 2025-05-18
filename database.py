53cfecceb763f113bf083ae49425606af9f536b8
Mark Sommer <m.a.sommer88@gmail.com>
2025-05-18 12:26:10 +0200

    Changed input format
    Changed input formatting for db data

--- database.py
+++ database.py
@@ -53,7 +53,7 @@ class Database:
                 VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)
                 ON CONFLICT (guild_id, listename, user_id, charname)
                 DO UPDATE SET spec=$5, class=$6, ilvl=$7, rio=$8, rolle=$9
-            """, guild_id, listename, user_id, charname, spec, class_, ilvl, rio, rolle)
+            """, str(guild_id), listename, str(user_id), charname, spec, class_, ilvl, rio, rolle)
 
     async def fjern_registrering(self, guild_id, listename, user_id, charname=None):
         async with self.pool.acquire() as conn:
@@ -61,12 +61,12 @@ class Database:
                 await conn.execute("""
                     DELETE FROM registreringer
                     WHERE guild_id=$1 AND listename=$2 AND user_id=$3 AND charname=$4
-                """, guild_id, listename, user_id, charname)
+                """, str(guild_id), listename, str(user_id), charname)
             else:
                 await conn.execute("""
                     DELETE FROM registreringer
                     WHERE guild_id=$1 AND listename=$2 AND user_id=$3
-                """, guild_id, listename, user_id)
+                """, str(guild_id), listename, str(user_id))
 
     async def hent_alle(self, guild_id, listename):
         async with self.pool.acquire() as conn:
@@ -74,14 +74,14 @@ class Database:
                 SELECT * FROM registreringer
                 WHERE guild_id=$1 AND listename=$2
                 ORDER BY rolle, charname
-            """, guild_id, listename)
+            """, str(guild_id), listename)
 
     async def get_liste(self, guild_id, listename):
         async with self.pool.acquire() as conn:
             result = await conn.fetchrow("""
                 SELECT * FROM lister
                 WHERE guild_id=$1 AND listename=$2
-            """, guild_id, listename)
+            """, str(guild_id), listename)
             return dict(result) if result else None
 
     async def opret_liste(self, guild_id, listename, titel, besked_id, ejer_id):
@@ -91,14 +91,14 @@ class Database:
                 VALUES ($1,$2,$3,$4,$5)
                 ON CONFLICT (guild_id, listename)
                 DO UPDATE SET titel=$3, besked_id=$4, ejer_id=$5
-            """, guild_id, listename, titel, besked_id, ejer_id)
+            """, str(guild_id), listename, titel, str(besked_id), str(ejer_id))
 
     async def slet_liste(self, guild_id, listename):
         async with self.pool.acquire() as conn:
             await conn.execute("""
                 DELETE FROM lister
                 WHERE guild_id=$1 AND listename=$2
-            """, guild_id, listename)
+            """, str(guild_id), listename)
 
     async def opdater_besked_id(self, guild_id, listename, besked_id):
         async with self.pool.acquire() as conn:
@@ -106,4 +106,4 @@ class Database:
                 UPDATE lister
                 SET besked_id=$3
                 WHERE guild_id=$1 AND listename=$2
-            """, guild_id, listename, besked_id)
+            """, str(guild_id), listename, str(besked_id))
